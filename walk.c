9e7d052bc8ed73190df2f137aeaed6c199693c45
Darius Makaitis <dmakaitis@cox.net>
2021-02-09 15:50:17 -0600

    Last (non-functional) update from 1992. Started figuring out how to allocate memory. Attempted to try using blitter to speed up draw time

--- walk.c
+++ walk.c
@@ -1,19 +1,21 @@
-/****************/
-/*              */
-/* Walking Test */
-/*              */
-/* Version 1.5  */
-/*              */
-/* 09/29/91     */
-/*              */
-/****************/
+/************************/
+/*                      */
+/*  Walking Test        */
+/*                      */
+/*  Version 2.0         */
+/*                      */
+/*  02/12/92            */
+/*                      */
+/************************/
 
 #include    <exec/types.h>
+#include    <exec/memory.h>
 #include    <intuition/intuition.h>
 #include    <graphics/gfx.h>
 #define     MAPSIZE 32
-
-int UpdateCntr;
+#define     SWIDE   320
+#define     SHIGH   320
+#define     SDEEP   5
 
 char    map[33] [33] = {
             "*******..................*******",
@@ -48,7 +50,7 @@ char    map[33] [33] = {
             "*.....*...*****...*****..*.....*",
             "*.....*..................*.....*",
             "*******..................*******",
-           };
+            };
 
 /************************/
 /*                      */
@@ -77,64 +79,54 @@ USHORT  Colors[] =
 
 USHORT  Wall[] =
 {
-    0xffff, 0xbf77, 0xfbbf, 0xffff,
-    0x6fda, 0xdf7f, 0xfd83, 0xffff,
-    0xd97f, 0x9933, 0xffef, 0xefff,
-    0xefdf, 0x96ee, 0xffff, 0x0000,
+    0xffff, 0xffff, 0xffff, 0x0000, 0xffff,
+    0xbf77, 0x9375, 0x6c8e, 0x0000, 0xffff,
+    0xfbff, 0xdb83, 0x6c7c, 0x0000, 0xffff,
+    0xffff, 0x01b6, 0xfe49, 0x0000, 0xffff,
     
-    0xffff, 0x9375, 0xdb83, 0x01b6,
-    0xfd27, 0x9341, 0xb6cb, 0xffff,
-    0x0056, 0x9131, 0xda27, 0xb4f7,
-    0xd9cf, 0xd935, 0xffff, 0x0000,
+    0x6fda, 0xfd27, 0x02dc, 0x0000, 0xffff,
+    0xdf7f, 0x9341, 0x6cbe, 0x0000, 0xffff,
+    0xfd83, 0xb6cb, 0x4934, 0x0000, 0xffff,
+    0xffff, 0xffff, 0xffff, 0x0000, 0xffff,
     
-    0xffff, 0x6c8e, 0x6c7c, 0xfe49,
-    0x02dc, 0x6cbe, 0x4934, 0xffff,
-    0xffad, 0x6ece, 0x25d8, 0x4b08,
-    0x2630, 0x0000, 0xffff, 0x0000,
+    0xd97f, 0x0056, 0xffad, 0x0000, 0xffff,
+    0x9933, 0x9131, 0x6ece, 0x0000, 0xffff,
+    0xffef, 0xda27, 0x25d8, 0x0000, 0xffff,
+    0xefff, 0xb4f7, 0x4b08, 0x0000, 0xffff,
     
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-        
-    0xffff, 0xffff, 0xffff, 0xffff,
-    0xffff, 0xffff, 0xffff, 0xffff,
-    0xffff, 0xffff, 0xffff, 0xffff,
-    0xffff, 0xffff, 0xffff, 0xffff
+    0xefdf, 0xd9cf, 0x2630, 0x0000, 0xffff,
+    0x96ee, 0xd935, 0x0000, 0x0000, 0xffff,
+    0xffff, 0xffff, 0xffff, 0x0000, 0xffff,
+    0x0000, 0x0000, 0x0000, 0x0000, 0xffff
 };
 
 /************************/
 /*                      */
-/*  Grass Gfx Data      */
+/*  Floor Gfx Data      */
 /*                      */
 /************************/
 
 USHORT  Grass[] =
 {
-    0x5C00, 0xA67C, 0x513C, 0x286C,
-    0x241C, 0x99BC, 0x46FC, 0x227C,
-    0x10BC, 0x447C, 0x44BC, 0x52DC,
-    0x78BC, 0x7D7C, 0x0002, 0x0000,
+    0x5C00, 0x33FF, 0x0000, 0xFFFE, 0x0000,
+    0xa67c, 0x19ff, 0x0000, 0xfffe, 0x0000,
+    0x513c, 0x8eff, 0x0000, 0xfffe, 0x0000,
+    0x286c, 0xc77f, 0x0000, 0xfffe, 0x0000,
     
-    0x33FF, 0x19FF, 0x8EFF, 0xC77F,
-    0xCBFF, 0x67FF, 0xBDFF, 0xDDFF,
-    0xEF7F, 0xFCFF, 0xFBFF, 0xFDFF,
-    0xFFFF, 0xFFFF, 0xFFFD, 0xFFFE,
+    0x241c, 0xcbff, 0x0000, 0xfffe, 0x0000,
+    0x99bc, 0x67ff, 0x0000, 0xfffe, 0x0000,
+    0x46fc, 0xbdff, 0x0000, 0xfffe, 0x0000,
+    0x227c, 0xddff, 0x0000, 0xfffe, 0x0000,
     
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
+    0x10bc, 0xef7f, 0x0000, 0xfffe, 0x0000,
+    0x447c, 0xfcff, 0x0000, 0xfffe, 0x0000,
+    0x44bc, 0xfbff, 0x0000, 0xfffe, 0x0000,
+    0x52dc, 0xfdff, 0x0000, 0xfffe, 0x0000,
     
-    0xFFFE, 0xFFFE, 0xFFFE, 0xFFFE,
-    0xFFFE, 0xFFFE, 0xFFFE, 0xFFFE,
-    0xFFFE, 0xFFFE, 0xFFFE, 0xFFFE,
-    0xFFFE, 0xFFFE, 0xFFFE, 0x0001,
-    
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000
+    0x78bc, 0xffff, 0x0000, 0xfffe, 0x0000,
+    0x7d7c, 0xffff, 0x0000, 0xfffe, 0x0000,
+    0x0002, 0xfffd, 0x0000, 0xfffe, 0x0000,
+    0x0000, 0xfffe, 0x0000, 0x0001, 0x0000
 };
 
 /************************/
@@ -145,30 +137,25 @@ USHORT  Grass[] =
 
 USHORT  Avatar[] =
 {
-    0x0000, 0x0800, 0x1000, 0x28A0,
-    0x1FF0, 0x3718, 0x1260, 0x0980,
-    0x0030, 0x0A90, 0x0158, 0x015C,
-    0x006C, 0x006C, 0x0000, 0x0000,
+    0x0000, 0x0800, 0x1000, 0x28A0, 0x1FF0,
+    0x3718, 0x1260, 0x0980, 0x0030, 0x0A90, 
+    0x0158, 0x015C, 0x006C, 0x006C, 0x0000, 
+    0x0000, 0x7C00, 0x7E00, 0xFFE0, 0xF430,
     
-    0x7C00, 0x7E00, 0xFFE0, 0xF430,
-    0x6218, 0x5EC4, 0x47E4, 0x6F84,
-    0x707C, 0x37FE, 0x1FEE, 0x0FA6,
-    0x01B6, 0x00FE, 0x006C, 0x0000,
+    0x6218, 0x5EC4, 0x47E4, 0x6F84, 0x707C, 
+    0x37FE, 0x1FEE, 0x0FA6, 0x01B6, 0x00FE, 
+    0x006C, 0x0000, 0x3000, 0x3C00, 0x7800, 
+    0x7380, 0x2380, 0x2FC0, 0x1FE0, 0x0BE0,
     
-    0x3000, 0x3C00, 0x7800, 0x7380,
-    0x2380, 0x2FC0, 0x1FE0, 0x0BE0,
-    0x03E0, 0x01B0, 0x00D8, 0x005C,
-    0x006C, 0x006C, 0x0000, 0x0000,
+    0x03E0, 0x01B0, 0x00D8, 0x005C, 0x006C, 
+    0x006C, 0x0000, 0x0000, 0x3000, 0x3C00, 
+    0x7800, 0x73E0, 0x23F0, 0x2FF8, 0x3FF8, 
+    0x1B98, 0x0C60, 0x09B0, 0x00C0, 0x0000,
     
-    0x3000, 0x3C00, 0x7800, 0x73E0,
-    0x23F0, 0x2FF8, 0x3FF8, 0x1B98,
-    0x0C60, 0x09B0, 0x00C0, 0x0000,
-    0x0000, 0x0000, 0x0000, 0x0000,
-    
-    0x0000, 0x0000, 0x0000, 0x0860,
-    0x1E70, 0x0E38, 0x2618, 0x1278,
-    0x0F80, 0x0800, 0x0018, 0x005C,
-    0x006C, 0x006C, 0x0000, 0x0000
+    0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 
+    0x0000, 0x0000, 0x0860, 0x1E70, 0x0E38, 
+    0x2618, 0x1278, 0x0F80, 0x0800, 0x0018, 
+    0x005C, 0x006C, 0x006C, 0x0000, 0x0000
 };
     
 /********************************/
@@ -205,68 +192,60 @@ USHORT  Chr[] =
     0x0000, 0x0000, 0x0000, 0x0000
 };
 
-/************************/  
+/************************/
 /*                      */
-/*  Chr Sil Buffer      */
+/*  Chr Sil Biffer      */
 /*                      */
 /************************/
 
-USHORT  Sil[]   =
+USHORT  Sil[] =
 {
     0x0000, 0x0000, 0x0000, 0x0000,
     0x0000, 0x0000, 0x0000, 0x0000,
     0x0000, 0x0000, 0x0000, 0x0000,
     0x0000, 0x0000, 0x0000, 0x0000
 };
-    
-struct  GfxBase     *GfxBase;
-struct  IntuitionBase   *IntuitionBase;
-struct  Window      *FirstWindow;
-struct  Window      *FirstWindow2;
-struct  Screen      *GameScreen1;
-struct  Screen      *GameScreen2;
-struct  ViewPort    *GamePort;
 
-struct  NewScreen   NewGameScreen =
+ULONG   Output;
+
+struct  BitMap      *allocbitmap(LONG, LONG, LONG);
+
+/* struct   BitMap      Sil, Chr, Avatar, Grass, Wall; */
+struct  IntuitionBase   *IntuitionBase;
+struct  GfxBase     *GfxBase;
+struct  Window      *FirstWindow;
+struct  Screen      *GameScreen;
+struct  BitMap      *GameMap;
+
+struct  NewScreen   NewGameScreen = 
 {
-    0, 0,
-    320, 200,
-    5,
+    0, 0, 
+    SWIDE, SHIGH, 
+    SDEEP,
     7, 4,
     NULL,
-    CUSTOMSCREEN,
+    CUSTOMSCREEN | CUSTOMBITMAP,
     NULL,
-    (UBYTE *)"Ultima Walking Test, V. 1.5",
+    (UBYTE *) "Ultima Walking Test, V. 2.0",
     NULL,
-    NULL,
+    NULL
 };
 
 struct  NewWindow   FirstNewWindow =
 {
-    0, 0,                                       /* XPos, YPos       */
-    200, 200,                                   /* Width, Height    */
-    8, 10,                                      /* Pen Colors       */
-    RAWKEY | CLOSEWINDOW,                       /* IDCME Flags      */
-    SMART_REFRESH | ACTIVATE | WINDOWDEPTH |    /* Window Flags     */ 
-    WINDOWDRAG | WINDOWCLOSE,
-    NULL,                                       /* Gadget Ptr       */
-    NULL,                                       /* Chk Mrk Gfx Ptr  */
-    (UBYTE *) "Walk 1.5",                       /* Window Title Ptr */
-    NULL,                                       /* Screen Ptr       */
-    NULL,                                       /* BitMap Ptr       */
-    200, 200,                                   /* Min Width/Height */
-    320, 200,                                   /* Max Width/Height */
-    CUSTOMSCREEN,                               /* Window Type      */
-};
-
-struct  Image   Output =
-{
-    0, 0,       /* Left, Top Edge       */
-    16, 16,     /* Width, Height        */
-    5,          /* Depth (Bit Planes)   */
-    &Avatar[0], /* Image Ptr            */
-    31, 0,      /* Plane Pick, On/Off   */
-    NULL        /* Next Image Prt       */
+    0, 0,
+    10, 10,
+    8, 10,
+    RAWKEY | CLOSEWINDOW,
+    SMART_REFRESH | ACTIVATE | WINDOWDEPTH | WINDOWDRAG | WINDOWCLOSE,
+    NULL,
+    NULL,
+    (UBYTE *) "Walk 2.0",
+    NULL,
+    NULL,
+    10, 10,
+    320, 200,
+    CUSTOMSCREEN,
 };
 
 struct  IntuiMessage    *message;
@@ -277,7 +256,7 @@ struct  IntuiMessage    *message;
 /*                      */
 /************************/
 
-void    main()
+void main()
 
 {
     int x = 16;
@@ -285,14 +264,12 @@ void    main()
     int tx, ty;
     char    code;
     ULONG   MessageClass;
-                
+    
     Open_All();
     
     FOREVER
     {
-        ActivateWindow(FirstWindow);
-        
-        draw(x,y);
+        draw(x, y);
         
         tx = x;
         ty = y;
@@ -373,7 +350,7 @@ void    main()
             else
             {
                 y = ty;
-                x = tx;
+                x = tx; 
             }                   
         }
         
@@ -381,177 +358,175 @@ void    main()
     }
 }
 
-
 /****************************************/
 /*                                      */
-/* Draw Screen                          */
+/* Draw Screen V. 2.0                   */
 /*                                      */
-/* draw(x,y) : x = XPos, Y = YPos       */
+/* draw(x,y) : x = XPos, y = YPos       */
 /*                                      */
 /****************************************/
 
 draw(x,y)
 
-register int x, y;
+register int    x, y;
 
 {
-    struct  RastPort    *MyWindowsRastPort;
+    int xx, yy, cx, cy, dx, dy, Ox, Oy, Oh;
     
-    int     xx, yy, cx, cy, dx, dy;
-    USHORT  *ChrBakImageData;
-
-    UpdateCntr = UpdateCntr ^ 2;
-    
-    if(UpdateCntr && 2)
-    
+    for(dy = y - 4, cy = 0; dy <= y + 4; dy++, cy++)
     {
-        UpdateCntr = UpdateCntr ^ 1;
+        yy = dy;
         
-        if(UpdateCntr) MyWindowsRastPort = FirstWindow->RPort;
-        else MyWindowsRastPort = FirstWindow2->RPort;
+        if(yy < 0) yy = yy + MAPSIZE;
+        if(yy > MAPSIZE-1) yy = yy - MAPSIZE;
         
-    /*  printf("\n%d, %d\n", x, y); */
-        for(dy = y - 4, cy = 0; dy <= y + 4; dy++, cy++)
+        for(dx = x - 4, cx = 0; dx <= x + 4; dx++, cx++)
         {
-            yy = dy;
+            xx = dx;
             
-            if(yy < 0) yy = yy + MAPSIZE;
-            if(yy > MAPSIZE-1) yy = yy - MAPSIZE;
-        
-            for(dx = x - 4, cx = 0; dx <= x + 4; dx++, cx++)
-            {
-                xx = dx;
-                
-                if(xx < 0) xx = xx + MAPSIZE;
-                if(xx > MAPSIZE-1) xx = xx - MAPSIZE;
-                
-                if(map[yy] [xx] == '*') Output.ImageData = &Wall[0];
-                if(map[yy] [xx] == '.') Output.ImageData = &Grass[0];
-                if(xx == x && yy == y)
-                {
-                    ChrBakImageData = Output.ImageData;
-                    DrawPlayer(ChrBakImageData);
-                }
-                            
-                Output.LeftEdge = (5 + 16 * cx);
-                Output.TopEdge = (5 + 16 * cy);
-                DrawImage(MyWindowsRastPort, &Output, 10L, 10L);
-    /*          printf("%c", map[yy] [xx]); */
-            }
-            DrawImage(MyWindowsRastPort, &Output, 10L, 10L);
+            if(xx < 0) xx = xx + MAPSIZE;
+            if(xx > MAPSIZE-1) xx = xx - MAPSIZE;
             
-            if(UpdateCntr) ScreenToFront(GameScreen1);
-            else ScreenToFront(GameScreen2);
-                
-/*          printf("  %d, %d\n", xx, yy);   */
+            if(map[yy] [xx] == '*') Output = &Wall[0];
+            if(map[yy] [xx] == '.') Output = &Grass[0];
+            
+    /* Chr Display here.........*/
+    
+            Ox = (8 + 16 * cx);
+            Oy = 5 * (8 + 16 * cy);
+            BltBitMap (Output, 0, 0, GameMap, Ox, Oy, 16, 16 * Oh, 0xc0, -1, NULL);
         }
     }
-}   
-
-/********************************/
-/*                              */
-/* Open Intuition and Window    */
-/*                              */
-/********************************/
-
-Open_All()
-{
-    void    *OpenLibrary();
-    struct  Window  *OpenWindow();
-    
-    if (!(IntuitionBase = (struct IntuitionBase *) OpenLibrary("intuition.library", 0L)))
-    {
-        printf("Intuition Library not found!\n");
-        Close_All(FALSE);
-    }
-    
-    if (!(GfxBase = (struct GfxBase *) OpenLibrary("graphics.library", 0L)))
-    {
-        printf("Graphics Library not found!\n");
-        Close_All(FALSE);
-    }
-    
-    if(!(GameScreen1 = (struct Screen *) OpenScreen(&NewGameScreen)))
-    {
-        printf("Screen cannot be opened!\n");
-        Close_All(FALSE);
-    }
-    
-    FirstNewWindow.Screen = GameScreen1;
-    
-    if (!(FirstWindow = (struct Window *) OpenWindow(&FirstNewWindow)))
-    {
-        printf("Window will not open!\n");
-        Close_All(FALSE);
-    }
-    
-    if(!(GameScreen2 = (struct Screen *) OpenScreen(&NewGameScreen)))
-    {
-        printf("Screen cannot bo opened!\n");
-        Close_All(FALSE);
-    }
-    
-    FirstNewWindow.Screen = GameScreen2;
-    
-    if(!(FirstWindow2 = (struct Window *) OpenWindow(&FirstNewWindow)))
-    {
-        Close_All(FALSE);
-    }
-    
-    GamePort = (struct ViewPort *) ViewPortAddress(FirstWindow);
-    LoadRGB4(GamePort, &Colors[0], 32);
-    
-    GamePort = (struct ViewPort *) ViewPortAddress(FirstWindow2);
-    LoadRGB4(GamePort, &Colors[0], 32);
 }
 
-/********************************/
-/*                              */
-/* Close Intuition and Window   */
-/*                              */
-/********************************/
+/****************/
+/*              */
+/* Open up...   */
+/*              */
+/****************/
+
+Open_All()
+
+{
+    void        *OpenLibrary();
+    struct      Window      *OpenWindow();
+    struct      BitMap      ScrMap;
+    register    PLANEPTR    base;
+    register    short       i;
+    
+    if(!(IntuitionBase = (struct IntuitionBase *) OpenLibrary("intuition.library", 0L)))
+        Close_All(FALSE);
+        
+    if(!(GfxBase = (struct GfxBase *) OpenLibrary("graphics.library", 0L)))
+        Close_All(FALSE);
+    
+    if(!(GameMap = allocbitmap(1, SWIDE, SHIGH * SDEEP)))
+        Close_All(FALSE);
+    
+    InitBitMap(&ScrMap, SDEEP, SWIDE, SHIGH);
+    ScrMap.BytesPerRow *= SDEEP;
+    base = GameMap->Planes[0];
+    for(i = 0; i < SDEEP; i++)
+    {
+        ScrMap.Planes[i] = base;
+        base += GameMap->BytesPerRow;
+    }
+    
+    NewGameScreen.CustomBitMap = &ScrMap;
+    if(!(GameScreen = (struct Screen *) OpenScreen(&NewGameScreen)))
+        Close_All(FALSE);
+    
+    FirstNewWindow.Screen = GameScreen;
+    
+    if(!(FirstWindow = (struct Window *) OpenWindow(&FirstNewWindow)))
+        Close_All(FALSE);
+    
+    LoadRGB4((struct ViewPort *) ViewPortAddress(FirstWindow), &Colors[0], 32);
+}
+
+/****************/
+/*              */
+/* Close All... */
+/*              */
+/****************/
 
 Close_All(tf)
 
 register    int tf;
 
 {
-    if (FirstWindow2)   CloseWindow(FirstWindow2);
-    if (FirstWindow)    CloseWindow(FirstWindow);
-    if (GameScreen2)    CloseScreen(GameScreen2);
-    if (GameScreen1)    CloseScreen(GameScreen1);
-    if (GfxBase)        CloseLibrary(GfxBase);
-    if (IntuitionBase)  CloseLibrary(IntuitionBase);
-
+    if(FirstWindow)     CloseWindow(FirstWindow);
+    if(GameScreen)      CloseScreen(GameScreen);
+    if(GameMap)     freebitmap(GameMap);
+    if(GfxBase)     CloseLibrary(GfxBase);
+    if(IntuitionBase)   CloseLibrary(IntuitionBase);
+    
     exit(tf);
 }
 
-/************************/
-/*                      */
-/*  Draw Players        */
-/*  V1.1                */
-/*                      */
-/*  DrawPlayer(o)       */
-/*                      */
-/************************/
+/****************/
+/*              */
+/* allocbitmap  */
+/*              */
+/****************/
 
-DrawPlayer(o)
+struct  BitMap  *
 
-register    USHORT  *o;
+allocbitmap(deep, wide, high)
+
+register    LONG    deep, wide, high;
 
 {
-    int c, c2;
+    register    struct  BitMap  *bm;
+    register    LONG        size;
     
-    for(c = 0; c <= 15; c++)
+    if (bm = AllocMem (sizeof (*bm), MEMF_CLEAR))
     {
-        Sil[c] = ~(Avatar[c] | Avatar[16 + c] | Avatar[32 + c] |
-               Avatar[48 + c] | Avatar[64 + c]);
-        for(c2 = 0; c2 <= 64; c2 = c2 + 16)
-            Chr[c + c2] = (*(o + c + c2) & Sil[c]) | Avatar[c + c2];
+        InitBitMap(bm, deep, wide, high);
+        size = (LONG) bm->BytesPerRow * bm->Rows;
+        
+        while (deep--)
+            if(!(bm->Planes[deep] = AllocMem(size, MEMF_CHIP)))
+            {
+                freebitmap(bm);
+                return(NULL);
+            }
+        
+        BltBitMap(bm, 0, 0, bm, 0, 0, wide, high, 0, -1, NULL);
     }
-    Output.ImageData = &Chr[0];
+    return(bm);
 }
 
+/****************/
+/*              */
+/* freebitmap   */
+/*              */
+/****************/
 
+freebitmap(bm)
+
+register    struct  BitMap  *bm;
+
+{
+    register    PLANEPTR    *p;
+    register    LONG        size;
+    register    short       deep;
+    
+    size = (LONG) bm->BytesPerRow * bm->Rows;
+    p = bm->Planes;
+    deep = bm->Depth;
+    
+    while(deep--)
+    {
+        if (*p)
+        {
+            FreeMem(*p, size);
+            *p = NULL;
+        }
+        p++;
+    }
+    bm->Depth = 0;
+    FreeMem(bm, sizeof(*bm));
+}
 
 
