483e43842694d433945063058f17d23689c393db
Darius Makaitis <dmakaitis@cox.net>
2021-02-08 22:55:32 -0600

    Version 0.2

--- walk.c
+++ walk.c
@@ -1,57 +1,97 @@
 /****************/
-/*              */
-/* Walking Test */
-/*              */
-/* Version 0.1  */
-/*              */
-/* 09/25/91     */
-/*              */
+/*		*/
+/* Walking Test	*/
+/*		*/
+/* Version 0.2	*/
+/*		*/
+/* 09/25/91	*/
+/*		*/
 /****************/
 
-void    main()
+void	main()
 {
-    int x = 16;
-    int y = 16;
-    int exit = 0;
-    char    input;
-    
-    printf("\n%d, %d\n\n:");
-    do
-    {
-        exit = 1;
-        
-        scanf("%c", &input);
-        
-        if(input == 'i')
-        {
-            if(y > 0)
-                y = y - 1;
-        }
-        
-        if(input == 'k')
-        {
-            if(y < 32)
-                y = y + 1;
-        }
-        
-        if(input == 'j')
-        {
-            if(x > 0)
-                x = x - 1;
-        }
-        
-        if(input == 'l')
-        {
-            if(x < 32)
-                x = x + 1;
-        }
-        
-        if(input != 'q')
-        {
-            exit = 0;
-            printf("\n%d, %d\n\n:", x, y);
-        }
-    } while(exit == 0);
+	int	x = 8;
+	int	y = 8;
+	int	exit = 0;
+	char	input;
+				
+	draw(x,y);
+	do
+	{
+		exit = 1;
+		
+		scanf("%c", &input);
+		
+		if(input == 'i')
+		{
+			if(y > 2)
+				y = y - 1;
+		}
+		
+		if(input == 'k')
+		{
+			if(y < 13)
+				y = y + 1;
+		}
+		
+		if(input == 'j')
+		{
+			if(x > 2)
+				x = x - 1;
+		}
+		
+		if(input == 'l')
+		{
+			if(x < 13)
+				x = x + 1;
+		}
+		
+		if(input != 'q')
+		{
+			exit = 0;
+			draw(x,y);
+		}
+	} while(exit == 0);
 }
+
+
+draw(x,y)
+
+register int x, y;
+
+{
+	
+	char	map[16] [16] = {
+				"****************",
+				"****************",
+				"**..*****.**..**",
+				"***...***....***",
+				"***.*.*...**..**",
+				"**..*.*.*****.**",
+				"**.**.*..*....**",
+				"**....**...**.**",
+				"***.*****.**..**",
+				"***.*...*..*.***",
+				"**..*.*...*...**",
+				"**.**...***.****",
+				"**.******.....**",
+				"**........***.**",
+				"****************",
+				"****************",
+			       };
+
+	int 	xx, yy;
+
+	printf("\n%d, %d\n\n", x, y);
+	for(yy = y - 2; yy <= y + 2; yy++)
+	{
+		for(xx = x - 2; xx <= x + 2; xx++)
+			printf("%c", map[yy] [xx]);
+		printf("\n");
+	}
+	printf("\n%c\n:", map[x] [y]);
+}
+
+			
 
 
